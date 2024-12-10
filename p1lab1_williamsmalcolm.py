7aebb5b35cd2f8f6e892b27025f4814559aea092
malcolm344 <williamm9307@student.faytechcc.edu>
2024-12-09 20:37:38 -0500

    Add files via upload

---
+++ p1lab1_williamsmalcolm.py
@@ -0,0 +1,14 @@
+ # Malcolm Williams
+ # 12/9/24 
+ # p1ab1
+ # This assignment teaches the student how to create a Python program using IDLE. It shows how to get input from the user and display output.
+
+#  user's first name
+first_name = input("Enter your first name: ")
+
+# the user's last name
+last_name = input("Enter your last name: ")
+
+# Output the greeting message
+print(f"Hello, {first_name} {last_name}! Welcome to CTI-110")
+
